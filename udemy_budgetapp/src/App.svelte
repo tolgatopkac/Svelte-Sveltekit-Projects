e60aa034924652e2b1f7d23c3abdf8f22d03032a
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-14 18:23:43 +0300

    view dispacth

--- udemy_budgetapp/src/App.svelte
+++ udemy_budgetapp/src/App.svelte
@@ -4,6 +4,8 @@
   import TransactionCardList from "./lib/components/TransactionCardList.svelte";
   import TransactionHeader from "./lib/components/TransactionHeader.svelte";
   import TransactionTableList from "./lib/components/TransactionTableList.svelte";
+
+  let view = "table";
 </script>
 
 <div class="p-1">
@@ -11,20 +13,24 @@
   <Header />
   <div class="d-flex flex-column flex-sm-row">
     <!-- Side -->
-    <Sidebar />
+    <Sidebar {view} on:handleViewSelect={(e) => (view = e.detail)} />
     <div class="flex-grow-1 pt-3">
       <!-- Transaction Header -->
       <TransactionHeader />
-      <div class="w-75 m-auto">
+      <div class:hide={view === "table"}>
         <!-- Transaction Card List -->
-        <TransactionTableList />
+        <TransactionCardList />
       </div>
-      <div>
+      <div class:hide={view === "card"} class="w-75 m-auto">
         <!-- Transaction Table List -->
+        <TransactionTableList />
       </div>
     </div>
   </div>
 </div>
 
 <style>
+  .hide {
+    display: none;
+  }
 </style>
--- udemy_budgetapp/src/lib/components/Sidebar.svelte
+++ udemy_budgetapp/src/lib/components/Sidebar.svelte
@@ -1,11 +1,33 @@
 <script>
   import TransactionForm from "./TransactionForm.svelte";
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  const handleView = (viewSelection) => {
+    dispatch("handleViewSelect", viewSelection);
+  };
+
+  export let view;
 </script>
 
 <div class="d-flex flex-column mt-3 rounded p-3 bg-c5">
   <div class="d-flex flex-sm-column justify-content-around">
-    <div class="mb-2 cursor-pointer">Card View</div>
-    <div class="mb-2 cursor-pointer">Table View</div>
+    <div
+      class:selected={view === "card"}
+      on:click={() => handleView("card")}
+      class="mb-2 cursor-pointer"
+    >
+      Card View
+    </div>
+    <div
+      class:selected={view === "table"}
+      on:click={() => handleView("table")}
+      class="mb-2 cursor-pointer"
+    >
+      Table View
+    </div>
   </div>
 
   <div>
@@ -35,5 +57,12 @@
 
   .bg-c5 {
     background-color: #c5cbd8;
+  }
+
+  .selected {
+    color: red;
+    background-color: aliceblue;
+    border: 1px solid aqua;
+    border-radius: 50px;
   }
 </style>
