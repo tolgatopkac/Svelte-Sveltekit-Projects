69d562748a4361879579972359b83e8c3c6fc9c7
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-03-30 17:56:25 +0300

    fixed .env

--- sveltekit_authentication_firebase/src/lib/firebase/firebase.client.js
+++ sveltekit_authentication_firebase/src/lib/firebase/firebase.client.js
@@ -6,12 +6,12 @@ import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: import.meta.VITE_APIKEY,
-	authDomain: import.meta.VITE_AUTHDOMAIN,
-	projectId: import.meta.VITE_PROJECTID,
-	storageBucket: import.meta.VITE_STORAGEBUCKET,
-	messagingSenderId: import.meta.VITE_MESSAGINGSENDERID,
-	appId: import.meta.VITE_APPID
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
