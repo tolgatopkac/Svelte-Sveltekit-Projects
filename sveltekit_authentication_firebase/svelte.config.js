6de8fbdcded422dc63271a9af161b1d872194e8f
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-03-30 17:51:19 +0300

    setup sveltekit and firebase

---
+++ sveltekit_authentication_firebase/svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+
+		appDir: 'assets/app'
+	}
+};
+
+export default config;
