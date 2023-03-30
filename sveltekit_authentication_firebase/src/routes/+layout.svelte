10055f0267a869a429a0056c7f3c4e6936fe7f34
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-03-30 18:28:56 +0300

    add projects files

--- sveltekit_authentication_firebase/src/routes/+layout.svelte
+++ sveltekit_authentication_firebase/src/routes/+layout.svelte
@@ -1,1 +1,26 @@
-<slot />
+<script>
+	import { onMount } from 'svelte';
+	import { auth } from '../lib/firebase/firebase.client';
+	import { authStore } from '../lib/stores/authStore';
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			console.log(user);
+
+			authStore.update((curr) => {
+				return { ...curr, isLoading: false, currentUser: user };
+			});
+		});
+	});
+</script>
+
+<main class="mainContainer">
+	<slot />
+</main>
+
+<style>
+	.mainContainer {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
