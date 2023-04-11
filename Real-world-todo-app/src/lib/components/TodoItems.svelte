dff97bd2f1d111afcf77e206a03a24af032dff1d
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-04-12 00:24:26 +0300

    Create TodoItems.svelte

---
+++ Real-world-todo-app/src/lib/components/TodoItems.svelte
@@ -0,0 +1,19 @@
+<script>
+  import TodoItem from "./TodoItem.svelte";
+
+  function onDelete() {
+    alert("delete");
+  }
+</script>
+
+<div>
+  <TodoItem on:delete={onDelete} />
+</div>
+
+<style>
+  div {
+    display: flex;
+    gap: 15px;
+    flex-direction: column;
+  }
+</style>
