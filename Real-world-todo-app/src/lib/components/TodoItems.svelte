79f62755f9dc6f765e6c0a5226cba7baab91d382
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-04-12 00:49:48 +0300

    bind:data

--- Real-world-todo-app/src/lib/components/TodoItems.svelte
+++ Real-world-todo-app/src/lib/components/TodoItems.svelte
@@ -1,13 +1,34 @@
-<script>
+<script lang="ts">
+  import type { Task } from "../../model";
   import TodoItem from "./TodoItem.svelte";
 
-  function onDelete() {
-    alert("delete");
+  function onDelete(task: Task) {
+    items = items.filter((t: Task) => t.id !== task.id);
   }
+
+  let items: Task[] = [
+    {
+      id: "1",
+      title: "Task 1",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Task 2",
+      completed: false,
+    },
+    {
+      id: "3",
+      title: "Task 3",
+      completed: false,
+    },
+  ];
 </script>
 
 <div>
-  <TodoItem on:delete={onDelete} />
+  {#each items as task (task.id)}
+    <TodoItem bind:data={task} on:delete={() => onDelete(task)} />
+  {/each}
 </div>
 
 <style>
