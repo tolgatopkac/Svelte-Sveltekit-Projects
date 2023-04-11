7a148ab26a172c3e9f9f1025a822bd7f52d60f69
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-04-12 00:25:47 +0300

    Create TodoItem.svelte with data

---
+++ Real-world-todo-app/src/lib/components/TodoItem.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Task } from "../../model";
+  import TodoItemActions from "./TodoItemActions.svelte";
+
+  let data: Task = {
+    id: "1",
+    title: "Buy some cookies",
+    completed: false,
+  };
+</script>
+
+<div>
+  <span>{data.title}</span>
+  <TodoItemActions on:delete />
+</div>
+
+<style>
+  div {
+    border: 1px solid black;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
